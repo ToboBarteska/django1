3056d5e4431856493a6b198a35a8e8ec953bd019
ToboBarteska <tobo232@seznam.cz>
2025-03-18 12:03:41 +0100

    Delete lecture3 directory

--- lecture3/guessing_game/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class GuessingGameConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'guessing_game'
--- lecture3/hello/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path 
-
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("<str:promena>/<str:cena>",views.problem, name="problem"),
-    path("pohoda",views.pohoda, name="pohoda")
-]
